9d0fc9ca597b7ab1b2088994a646067198f9a96c
Ayushi Verma <ayushiverma844@gmail.com>
2024-12-04 14:02:35 +0000

    container config done

---
+++ 13_Pyramid.cpp
@@ -0,0 +1,28 @@
+//     1 
+//    1 2 
+//   1 2 3 
+//  1 2 3 4 
+// 1 2 3 4 5
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n= 5;
+
+    //print  
+    for(int i=1; i<=n ; i++){
+        //SPACES
+        for(int j=n-1;j>=i;j--){
+           cout<<" ";
+        }
+
+        //STARS
+        for(int j=1;j<=i;j++){
+            cout<<""<<j<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
