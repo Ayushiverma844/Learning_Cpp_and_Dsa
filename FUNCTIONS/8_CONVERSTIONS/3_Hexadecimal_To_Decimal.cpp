953eb2d5f5ee186235f281b2723264ff1f4a0bad
Ayushi Verma <ayushiverma844@gmail.com>
2024-12-06 08:30:00 +0000

    container config done

---
+++ FUNCTIONS/8_CONVERSTIONS/3_Hexadecimal_To_Decimal.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+//Function to convert hexadecimal no to decimal
+int hexadecimalTodecimal(string n){
+     int ans =0;
+     int x = 1;
+    
+     int s = n.size();  //lenght of string
+
+    for(int i= 0; i<s;i++){
+        if(n[i]>'F' || (n[i] >='a' && n[i] <= 'z')){
+            cout<<"Wrong input!!\n";
+            return 0;
+        }
+    }
+    
+    for(int i= s-1; i>=0;i--){
+        if(n[i] >='0' && n[i]<='9'){
+            ans += x * (n[i]-'0');
+        }else if (n[i] >= 'A' && n[i] <='F'){
+            ans +=  x * (n[i]-'A' + 10);    //[ex = B - A + 10 == 1 + 10 == 11 ]
+        }
+        x *= 16;
+    }
+
+     return ans;
+}
+
+
+
+int main(){
+    string num;
+    cout<<"Give a haxadecimal no: ";
+    cin>>num;
+
+    cout<<"Decimal of "<<num<<" is = "<<hexadecimalTodecimal(num);
+    cout<<"\n";
+    return 0;
+}
---
+++ FUNCTIONS/8_CONVERSTIONS/4_Decimal_To_Binary.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+// Function to convert decimal to binary
+int DecimalToBinary(int n){
+    int ans = 0;
+    int x = 1;  // x will represent the current power of 2
+
+    // Find the largest power of 2 less than or equal to n
+    while (x <= n) {
+        x *= 2;
+    }
+
+    // Now x is the smallest power of 2 greater than n, so we go one step back
+    x /= 2;
+
+    // Generate the binary representation
+    while (x > 0) {
+        int lastdigit = n / x;
+        n -= lastdigit * x;
+        x /= 2;
+        ans = ans * 10 + lastdigit;
+    }
+
+    return ans;
+}
+
+int main(){
+    int num;
+    cout << "Give a decimal no: ";
+    cin >> num;
+
+    cout << "Binary of " << num << " is = " << DecimalToBinary(num);
+    cout << "\n";
+    return 0;
+}
