3bc5935207acd1210bef060a0c292caa214119b9
Ayushi Verma <ayushiverma844@gmail.com>
2024-12-12 05:04:40 +0000

    container config done

---
+++ CPP/1_BASIC_PROBLEMS/7_Bit_Operators.cpp
@@ -0,0 +1,34 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Enter value of a and b = ";
+    cin >> a >> b;
+
+    // Bitwise AND '&'
+    cout << "Bitwise AND of a and b = " << (a & b) << endl;
+
+    // Bitwise OR '|'
+    cout << "Bitwise OR of a and b = " << (a | b) << endl;
+
+    // Bitwise XOR '^'
+    cout << "Bitwise XOR of a and b = " << (a ^ b) << endl;
+
+    // Bitwise left shift '<<'
+    //(a << 2) shift 2 places left
+    //left shift multiplies the original number by 2^n , where n is no of bits to shift
+
+    cout << "Bitwise Left Shift of a = " << (a << 2) << endl;
+    cout << "Bitwise Left Shift of b = " << (b << 2) << endl;
+
+    // Bitwise right shift '>>'
+    //(a >> 2) shift 2 places right
+    //right shift Divides the original number by 2^n , where n is no of bits to shift
+
+    cout << "Bitwise Right Shift of a = " << (a >> 2) << endl;
+    cout << "Bitwise Right Shift of b = " << (b >> 2) << endl;
+
+    return 0;
+}
