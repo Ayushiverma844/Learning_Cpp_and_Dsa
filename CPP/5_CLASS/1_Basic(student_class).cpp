5b53edf54abcdf358a44a96f03ccccc01c5023c8
Ayushi Verma <ayushiverma844@gmail.com>
2024-12-14 07:47:43 +0000

    container config done

---
+++ CPP/5_CLASS/1_Basic(student_class).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name[3];
+    int age[3];  
+    char gender[3];
+
+    // Function to input student details
+    void Detail() {
+        for (int i = 0; i < 3; i++) {
+            cout << "Enter name of student " << i + 1 << " : ";
+            getline(cin, name[i]);  // Read the full name (including spaces)
+            cout << "Enter age of student " << i + 1 << " : ";
+            cin >> age[i];  // Read the age
+            cin.ignore();  // Clear the input buffer to handle newline character after reading age
+            cout << "Enter Gender(M/F) of student " << i + 1 << " : ";
+            cin >> gender[i];  // Read the age
+            cin.ignore();  // Clear the input buffer to handle newline character after reading gender
+          
+            cout<<"\n";
+        }
+
+    }
+
+    // Function to display student details
+    void Display() {
+        for (int i = 0; i < 3; i++) {
+            cout << "Name of student " << i + 1 << " is: " << name[i] << endl;
+            cout << "Age of student " << i + 1 << " is: " << age[i] << endl;
+            cout << "Gender of student " << i + 1 << " is: " << gender[i] << endl;
+
+            cout<<"\n";
+        }
+    }
+};
+
+int main() {
+    Student obj;
+
+    obj.Detail();  // Input student details
+    cout<<"\n\n-----------------------------------\n\n";
+    obj.Display();  // Display student details
+
+    return 0;
+}
