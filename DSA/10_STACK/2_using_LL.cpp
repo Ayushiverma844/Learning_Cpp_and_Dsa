8feeab7ea1793c0e3acf6962102dde208b4969e2
Ayushi Verma <ayushiverma844@gmail.com>
2025-02-10 03:24:57 +0000

    container config done

---
+++ DSA/10_STACK/2_using_LL.cpp
@@ -0,0 +1,49 @@
+// STACK USING LINKED LIST
+
+            
+#include<iostream>
+#include<list>
+using namespace std;
+
+
+class stack{
+   list<int> ll;
+
+public:
+  
+  //push function
+   void push(int x){
+      ll.push_front(x);
+   }
+
+   //pop function
+   void pop(){
+     if(!ll.empty()){
+        ll.pop_front();
+     } else{
+        cout<<"Stack is empty, can't pop\n";
+     }
+   }
+
+   // top function
+   int top(){
+      if(!ll.empty()){
+        return ll.front();
+      } else{
+        cout<<"Stack is empty, can't get top\n";
+        return -1;
+      }
+   }
+   
+};
+
+int main(){
+    stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout<<"Top element is "<<s.top()<<endl;
+    s.pop();
+    cout<<"Top element after popping is "<<s.top()<<endl;
+    return 0;
+}
