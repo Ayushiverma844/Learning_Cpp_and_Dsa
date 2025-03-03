25e35045d572e54111c7055341461dc0db084ac9
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-03 03:33:31 +0000

    container config done

--- CPP/3__FUNCTIONS/CONVERSTIONS/6_Decimal_To_Hexadecimal.cpp
+++ CPP/3__FUNCTIONS/CONVERSTIONS/6_Decimal_To_Hexadecimal.cpp
@@ -1,4 +1,27 @@
-//
+//Problem Statement:
+
+// Write a program that converts a given decimal number to its hexadecimal equivalent.
+
+// Input:
+// A positive integer n (0 ≤ n ≤ 10^6).
+// Output:
+// The hexadecimal representation of the given decimal number n.
+
+// Functionality:
+// The program should read a decimal number from the user.
+// It should then convert the decimal number to its hexadecimal representation.
+// Finally, it should display the hexadecimal equivalent of the input decimal number.
+
+// For example:
+// Input: 255
+// Output: FF
+
+// Input: 16
+// Output: 10
+
+// Input: 0
+// Output: 0
+
 
 #include<iostream>
 using namespace std;
