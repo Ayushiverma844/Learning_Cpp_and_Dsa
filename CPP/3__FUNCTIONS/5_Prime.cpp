d33645ee9648c0c0787f3b81f8dc6d63b1f54be4
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-08 12:36:12 +0000

    container config done

--- CPP/3__FUNCTIONS/5_Prime.cpp
+++ CPP/3__FUNCTIONS/5_Prime.cpp
@@ -1,3 +1,13 @@
+//Problem Statement:
+// Write a C++ program to check whether a given number n is a prime number or not.
+
+// The program should:
+
+// Take an integer n as input from the user.
+// Check if n is prime using a function is_prime(int n), which:
+// Iterates from 2 to n-1 and checks if n is divisible by any number.
+// Returns true (1) if n is prime, otherwise false (0).
+// Print whether the number is prime or not prime based on the function's output.
 
 #include<iostream>
 using namespace std;
