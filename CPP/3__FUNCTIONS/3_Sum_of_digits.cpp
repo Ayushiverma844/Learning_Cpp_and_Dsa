f0fba88fa3a16f5cad2160826a417f3d043157a9
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-09 13:32:07 +0000

    container config done

--- CPP/3__FUNCTIONS/3_Sum_of_digits.cpp
+++ CPP/3__FUNCTIONS/3_Sum_of_digits.cpp
@@ -1,3 +1,20 @@
+// Problem Statement:
+// Write a C++ program to calculate the Sum of Digits of a given number.
+
+// Explanation:
+// The user inputs an integer  𝑛
+// The program extracts each digit of  𝑛
+// adds them together, and returns the sum.
+// Example:
+// Input: 143
+// Calculation: 
+// 1+4+3=8
+// Output: Sum of Digits of 143 = 8
+
+// Constraints:
+// n should be a positive integer.
+// If n=0, the sum should be 0.
+
 #include<iostream>
 using namespace std;
 
