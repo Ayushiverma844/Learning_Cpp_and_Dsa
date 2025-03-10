dbd72ba8b29c21017ac74f91edaadc6b342cb8cb
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-10 03:20:04 +0000

    container config done

--- CPP/3__FUNCTIONS/6_prime_no_from_1_to_n.cpp
+++ CPP/3__FUNCTIONS/6_prime_no_from_1_to_n.cpp
@@ -1,10 +1,15 @@
-
+//
 
 #include<iostream>
 using namespace std;
 
 //function to check prime
 bool is_prime(int n){
+  if (n < 2)  // 0 and 1 are not prime
+  return false;
+  if (n == 2) // 2 is the smallest prime
+  return true;
+  
   for(int i=2;i<n;i++){
     if(n%i==0)
       return 0; //false(not prime)
