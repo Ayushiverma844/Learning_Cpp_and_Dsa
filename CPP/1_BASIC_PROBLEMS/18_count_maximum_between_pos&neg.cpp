de869bc0e2ce53db8bfecea5726a622db0f90603
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-12 02:46:19 +0000

    container config done

---
+++ CPP/1_BASIC_PROBLEMS/18_count_maximum_between_pos&neg.cpp
@@ -0,0 +1,18 @@
+// Problem Statement:
+// Given an array of integers nums, count how many positive and negative numbers it contains. The goal is to return the maximum count between the positive and negative numbers. Zero is neither considered positive nor negative.
+
+// Example:
+// Input:
+// nums = [-3, -2, -1, 0, 1, 2, 3]
+
+// Output:
+// 3
+
+// Explanation:
+// The array contains 3 negative numbers (-3, -2, -1) and 3 positive numbers (1, 2, 3). The function returns 3 since the count of both positive and negative numbers is the same.
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
