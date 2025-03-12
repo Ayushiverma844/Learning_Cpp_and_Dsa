370212e7e56685bd57f1bee6489e4b49115995e9
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-12 02:49:26 +0000

    container config done

--- CPP/1_BASIC_PROBLEMS/18_count_maximum_between_pos&neg.cpp
+++ CPP/1_BASIC_PROBLEMS/18_count_maximum_between_pos&neg.cpp
@@ -16,3 +16,34 @@
 #include <vector>
 using namespace std;
 
+class Solution {
+    public:
+        int maximumCount(vector<int>& nums) {
+            int neg = 0;
+            int pos = 0;
+            int max_count = 0;
+            for(int i=0;i<nums.size();i++){
+                // 0 is neither  positive nor negative.
+                if(nums[i] < 0){
+                    // no is negative
+                 neg++;
+                }
+                else if(nums[i] > 0){
+                    // no is postive
+                    pos++;
+                }
+            }
+            return max(neg,pos);;
+        }
+    };
+
+    int main() {
+        vector<int> nums = {-3, -2, -1, 0, 1, 2, 3}; // Example input
+    
+        Solution obj; 
+        int result = obj.maximumCount(nums);
+    
+        cout << "Maximum count of positive or negative numbers: " << result << endl;
+    
+        return 0;
+    }
