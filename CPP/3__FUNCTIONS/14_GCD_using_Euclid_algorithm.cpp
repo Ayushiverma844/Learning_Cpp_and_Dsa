9bceeda124e549b987cf4d5c3bf72804e0b60abe
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-17 03:28:09 +0000

    container config done

--- CPP/3__FUNCTIONS/14_GCD_using_Euclid_algorithm.cpp
+++ CPP/3__FUNCTIONS/14_GCD_using_Euclid_algorithm.cpp
@@ -1,5 +1,24 @@
 //Euclid Algorithm to find GCD (greatest common factor)
 
+// Problem Statement:
+
+// Write a C++ program that takes two positive integers, a and b, as input and calculates the Greatest Common Divisor (GCD) and Least Common Multiple (LCM) of the two numbers using the Euclidean algorithm for GCD.
+
+// The program should:
+
+// Accept two integers as input.
+// Ensure that neither of the integers is zero; if either number is zero, display an error message and terminate.
+// Calculate the GCD of the two integers using the Euclidean algorithm.
+// Calculate the LCM of the two integers using the formula:
+// LCM (𝑎,𝑏) = 𝑎× 𝑏 / GCD(𝑎,𝑏)
+
+ 
+// Output both the GCD and LCM of the two integers.
+
+// Constraints:
+// Both a and b must be positive integers.
+
+
 #include<iostream>
 using namespace std;
 
