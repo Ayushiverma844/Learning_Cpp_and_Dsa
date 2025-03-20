9a580c547e7598873a4072988a1d395d48aefdfb
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-20 02:49:30 +0000

    container config done

--- CPP/4__STRING/4_Convert_to_Upper_Lower_case.cpp
+++ CPP/4__STRING/4_Convert_to_Upper_Lower_case.cpp
@@ -1,3 +1,14 @@
+// Problem Statement:
+// Write a C++ program that converts a given string to uppercase and then to lowercase without using built-in functions like toupper() or tolower(). The program should:
+
+// Take a predefined string as input.
+
+// Convert all lowercase letters ('a' to 'z') to uppercase ('A' to 'Z') using ASCII value manipulation.
+
+// Convert all uppercase letters ('A' to 'Z') to lowercase ('a' to 'z') using ASCII value manipulation.
+
+// Display the original string, the converted uppercase string, and the converted lowercase string.
+
 #include <iostream>
 #include <string>
 using namespace std;
