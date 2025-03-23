7d31c04de2ea58f38eca2b60591661194ddc2381
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-23 06:52:33 +0000

    container config done

--- CPP/4__STRING/7_Most_Frequent_character.cpp
+++ CPP/4__STRING/7_Most_Frequent_character.cpp
@@ -1,4 +1,17 @@
-// find the most frequent character in a string and print both the character and its frequency.
+// Problem Statement:
+
+// Given a string of lowercase English characters, the task is to find the most frequent character in the string and print both the character and its frequency. If multiple characters have the same highest frequency, the program should print the lexicographically smallest character among them.
+
+// Input:
+// A string str of lowercase English characters (with a length between 1 and 1000).
+
+// Output:
+// Print the frequency of the most frequent character and the character itself, separated by a space.
+
+// Example:
+// For the string "abhskaaasjdnczff", the output should be:
+// 4 a
+// In this example, the character 'a' appears the most times (4 times), so it is printed along with its frequency.
 
 #include <iostream>
 #include <algorithm>
