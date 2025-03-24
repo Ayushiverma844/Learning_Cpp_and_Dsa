342f5d153ed274e96ba9e19744b411043913469a
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-24 05:24:20 +0000

    container config done

--- DSA/2_2D_Array/8_Count_non_meeting_days.cpp
+++ DSA/2_2D_Array/8_Count_non_meeting_days.cpp
@@ -1,3 +1,34 @@
+//Problem Statement:
+
+// You are given a total number of days (days) and a list of meetings, where each meeting is represented as a pair of integers [start, end] indicating the start and end days of the meeting. The meetings may overlap, and you need to determine how many free days remain after accounting for the time occupied by all the meetings.
+
+// A day is considered occupied if it falls within any meeting's start and end dates. Meetings can be merged if they overlap or touch each other. A meeting from day x to day y occupies all days between x and y inclusively.
+
+// Input:
+// days (1 ≤ days ≤ 10^5): The total number of days.
+
+// meetings (1 ≤ meetings.length ≤ 10^5): A list of meetings, where each meeting is a pair [start, end], indicating the start and end dates of the meeting (1 ≤ start ≤ end ≤ days).
+
+// Output:
+// Return the number of free days, i.e., the number of days that are not occupied by any meeting.
+
+// Example:
+// days = 10
+// meetings = {{1, 3}, {2, 5}, {7, 8}, {9, 10}}
+
+// Output:
+// Number of free days: 4
+
+// Explanation:
+// The first two meetings {1, 3} and {2, 5} overlap, so they are merged into one meeting from day 1 to day 5.
+
+// The third meeting {7, 8} occupies days 7 and 8.
+
+// The fourth meeting {9, 10} occupies days 9 and 10.
+
+// Therefore, days 6 and 10 are free, resulting in 4 free days.
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
