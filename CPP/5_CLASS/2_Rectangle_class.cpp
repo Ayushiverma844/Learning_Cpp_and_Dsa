a2b16d33fa280f96c49ad108966012d3777657ef
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-27 13:16:32 +0000

    container config done

--- CPP/5_CLASS/2_Rectangle_class.cpp
+++ CPP/5_CLASS/2_Rectangle_class.cpp
@@ -1,3 +1,40 @@
+// Problem Statement:
+
+// Create a C++ program that models a rectangle using a class. The program should include the following functionalities:
+
+// Class Definition:
+
+// The class should have private data members for length and width.
+
+// Implement a public method to initialize the dimensions of the rectangle.
+
+// Methods:
+
+// A method to calculate and return the area of the rectangle.
+
+// A method to calculate and return the perimeter of the rectangle.
+
+// Main Function:
+
+// In the main() function, create an object of the Rectangle class.
+
+// Initialize the dimensions of the rectangle with the values 20.0 for length and 30.0 for width.
+
+// Output the area and perimeter of the rectangle.
+
+// The program should display:
+
+// Area of the rectangle.
+
+// Perimeter of the rectangle.
+
+// Sample Output:
+// Area of rectangle: 600
+// Perimeter of rectangle: 100
+
+
+
+
 #include <iostream>
 using namespace std;
 
