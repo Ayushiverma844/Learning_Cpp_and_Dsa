712c1de8f0cd4211a9bf2e4f84b55c485361b2c7
Ayushi Verma <ayushiverma844@gmail.com>
2025-03-28 04:07:23 +0000

    container config done

--- CPP/5_CLASS/3_Employee_class.cpp
+++ CPP/5_CLASS/3_Employee_class.cpp
@@ -1,3 +1,42 @@
+//Problem Statement: Employee Salary Management
+// You are required to implement a class Employee that performs basic operations related to an employee's salary. The class should allow:
+
+// Input Employee Details:
+
+// The user will enter the employee's name (string), ID (integer), and salary (double).
+
+// The salary cannot be negative. If a negative salary is entered, the program should prompt the user to re-enter a valid salary.
+
+// Increase Salary:
+
+// The user will enter a percentage by which the salary should be increased.
+
+// The percentage increase cannot be negative. If a negative value is entered, the program should prompt the user to enter again.
+
+ 
+// Display Employee Details:
+// Print the name, ID, and updated salary after the increment.
+
+// Constraints:
+// The name consists of alphanumeric characters and spaces (no special characters).
+
+// The ID is a positive integer.
+
+// The salary and increment percentage must be non-negative.
+
+// Example Input/Output
+// Input 1:
+// Enter name: Alice Johnson  
+// Enter id: 101  
+// Enter salary: 50000  
+// Enter percent increase in salary: 10  
+
+// Output 1:
+// Name of employee is: Alice Johnson  
+// Id of employee is: 101  
+// Salary after increase is: 55000  
+
+
 #include <iostream>
 using namespace std;
 
