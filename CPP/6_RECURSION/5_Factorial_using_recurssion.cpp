401f75cd107b4c0c21571db465a7bcf24157e927
Ayushi Verma <ayushiverma844@gmail.com>
2025-04-16 03:35:48 +0000

    container config done

--- CPP/6_RECURSION/5_Factorial_using_recurssion.cpp
+++ CPP/6_RECURSION/5_Factorial_using_recurssion.cpp
@@ -1,3 +1,25 @@
+// 🔹 Problem Statement: Calculate Factorial of a Number Using Recursion
+// Write a C++ program to calculate the factorial of a given non-negative integer using a recursive function.
+
+//  Requirements:
+// Accept an integer input n from the user.
+
+// Use a recursive function fact(int n) to compute the factorial.
+
+// Display the result in the format:
+// Factorial of n = result
+
+//  Note:
+// Factorial of 0 is 1.
+
+// Factorial of a number n is defined as:
+// n! = n × (n - 1) × (n - 2) × ... × 1
+
+//  Sample Input/Output:
+// Enter value= 5
+// Factorial of 5 = 120
+
+
 #include<iostream>
 using namespace std;
 
