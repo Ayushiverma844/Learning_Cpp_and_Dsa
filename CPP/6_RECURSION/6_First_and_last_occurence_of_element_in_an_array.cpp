2d16de34b298d1ce49060115c21e95c1722890a8
Ayushi Verma <ayushiverma844@gmail.com>
2025-04-17 03:27:03 +0000

    container config done

--- CPP/6_RECURSION/6_First_and_last_occurence_of_element_in_an_array.cpp
+++ CPP/6_RECURSION/6_First_and_last_occurence_of_element_in_an_array.cpp
@@ -1,3 +1,44 @@
+// Problem Statement:
+// Title: Find First and Last Occurrence of an Element in an Array Using Recursion
+
+// Description:
+
+// Write a C++ program to find the first and last occurrence of a given key in an integer array using recursive functions.
+
+// Implement two recursive functions:
+
+// first() to return the index of the first occurrence of the key.
+
+// last() to return the index of the last occurrence of the key.
+
+// If the key is not found in the array, return -1.
+
+// Input:
+
+// An array of integers.
+
+// A key element to search for.
+
+// Output:
+
+// Index of the first occurrence of the key.
+
+// Index of the last occurrence of the key.
+
+// If not found, print appropriate messages.
+
+// Example:
+
+// yaml
+// Copy
+// Edit
+// Input:  arr[] = {1, 2, 3, 4, 2, 5, 2}, key = 2  
+// Output:  
+// First occurrence of 2 is at index: 1  
+// Last occurrence of 2 is at index: 6
+
+
+
 #include<iostream>
 using namespace std;
 
