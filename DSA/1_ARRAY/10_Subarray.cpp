fbd4c574453d1780c37118c28d8cd6d2c42ca16c
Ayushi Verma <ayushiverma844@gmail.com>
2025-05-04 04:27:52 +0000

    container config done

--- DSA/1_ARRAY/10_Subarray.cpp
+++ DSA/1_ARRAY/10_Subarray.cpp
@@ -1,3 +1,37 @@
+// Problem Statement:
+
+// Title: Print All Subarrays of an Array
+
+// Description:
+// Write a C++ program to input an array of integers from the user and then print all possible subarrays of the array. A subarray is a contiguous part of an array. For an array of size n, the total number of subarrays will be n * (n + 1) / 2.
+
+// Input:
+
+// An integer n representing the size of the array.
+
+// n integers representing the elements of the array.
+
+// Output:
+
+// Print all subarrays, one per line.
+
+// Example:
+
+// Input:
+// enter size of array : 3
+// Enter
+// 1 2 3
+
+// Output:
+// 1
+// 1 2
+// 1 2 3
+// 2
+// 2 3
+// 3
+
+
+
 #include<iostream>
 using namespace std;
 
