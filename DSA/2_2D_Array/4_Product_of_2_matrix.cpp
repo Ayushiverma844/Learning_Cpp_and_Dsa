e1775adbff7eb0dd1f65036d20521f03a5a45ed2
Ayushi Verma <ayushiverma844@gmail.com>
2025-05-09 07:10:16 +0000

    container config done

--- DSA/2_2D_Array/4_Product_of_2_matrix.cpp
+++ DSA/2_2D_Array/4_Product_of_2_matrix.cpp
@@ -1,3 +1,39 @@
+// Problem Statement:
+
+// Title: Matrix Multiplication
+
+// Description:
+// Write a C++ program to perform matrix multiplication of two matrices. The first matrix has dimensions n1 × n2, and the second matrix has dimensions n2 × n3. Multiply them and display the resulting matrix of size n1 × n3.
+
+// Input:
+
+// Three integers n1, n2, and n3 representing the sizes of the matrices.
+
+// n1 × n2 integers for the first matrix.
+
+// n2 × n3 integers for the second matrix.
+
+// Output:
+
+// Print the product of the two matrices.
+
+// Example:
+
+// Input:
+// Enter size: 2 3 2  
+// Enter values of 1st matrix:  
+// 1 2 3  
+// 4 5 6  
+// Enter values of 2nd matrix:  
+// 7 8  
+// 9 10  
+// 11 12  
+
+// Output:
+// Product of two matrix :  
+//   58  64  
+//  139 154
+
 #include<iostream>
 using namespace std;
 
