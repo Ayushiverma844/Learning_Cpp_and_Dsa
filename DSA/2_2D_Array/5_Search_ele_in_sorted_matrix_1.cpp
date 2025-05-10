555b3238c8c2dc323fe9c81ac38ac3659d8370ca
Ayushi Verma <ayushiverma844@gmail.com>
2025-05-10 04:43:10 +0000

    container config done

--- DSA/2_2D_Array/5_Search_ele_in_sorted_matrix_1.cpp
+++ DSA/2_2D_Array/5_Search_ele_in_sorted_matrix_1.cpp
@@ -1,4 +1,47 @@
 
+//  Problem Title: Search in a Row and Column-Wise Sorted Matrix
+// Problem Statement:
+
+// You are given a 2D matrix of size n x m where each row and each column is sorted in increasing order. Write a program to efficiently search for a given target value in the matrix.
+
+// If the target value is present in the matrix, print "element found."
+// Otherwise, print "Element does not exit."
+
+// Input Format:
+
+// First line: Two integers n and m — number of rows and columns.
+
+// Next n × m integers: Elements of the matrix.
+
+// Last line: An integer target — the value to search for.
+
+// Constraints:
+
+// 1 ≤ n, m ≤ 1000
+
+// -10⁵ ≤ matrix[i][j], target ≤ 10⁵
+
+// The matrix is sorted such that:
+
+// Each row is sorted from left to right.
+
+// Each column is sorted from top to bottom.
+
+// Output Format:
+
+// Print "element found." if the target is in the matrix.
+
+// Otherwise, print "Element does not exit."
+
+// Example Input:
+// 3 4
+// 1 4 7 11
+// 2 5 8 12
+// 3 6 9 16
+// 5
+
+// Example Output:
+// element found.
 
                
 
