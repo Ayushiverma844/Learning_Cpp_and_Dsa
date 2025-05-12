01ca8c260c8374d99c4e38e72229641472eb1014
Ayushi Verma <ayushiverma844@gmail.com>
2025-05-12 05:02:58 +0000

    container config done

--- DSA/2_2D_Array/7_Diffrence_of_daigonals.cpp
+++ DSA/2_2D_Array/7_Diffrence_of_daigonals.cpp
@@ -1,3 +1,29 @@
+//  Problem Statement: Absolute Diagonal Difference in a Square Matrix
+// Write a C++ program that calculates the absolute difference between the sums of the primary diagonal and the secondary diagonal of a square matrix of size n x n.
+
+//  Input:
+// An integer n representing the size of the square matrix.
+
+// n rows of n integers each, representing the matrix elements.
+
+// Output:
+// Print the absolute difference between the primary and secondary diagonal sums.
+
+// Example:
+// Input:
+
+// Enter matrix size (n x n): 3  
+// Enter matrix elements row-wise:  
+// 11 2 4  
+// 4 5 6  
+// 10 8 -12  
+// Primary Diagonal: 11, 5, -12 → sum = 4
+// Secondary Diagonal: 4, 5, 10 → sum = 19
+// Absolute Difference: |4 - 19| = 15
+
+// Output:
+// Diagonal Difference: 15
+
 #include<iostream>
 #include<vector>
 #include<cmath>   //for absolute diffrence
