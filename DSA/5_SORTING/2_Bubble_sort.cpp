25ac0fd96a99f0ba3b60b871292f97036c1af819
Ayushi Verma <ayushiverma844@gmail.com>
2025-05-18 06:51:04 +0000

    container config done

--- DSA/5_SORTING/2_Bubble_sort.cpp
+++ DSA/5_SORTING/2_Bubble_sort.cpp
@@ -1,3 +1,36 @@
+//  Problem Statement:
+// Title: Sort an Array using Bubble Sort
+
+// Objective:
+// Write a C++ program that accepts an array of integers from the user, sorts the array in ascending order using the Bubble Sort algorithm, and displays both the original and sorted arrays.
+
+// Requirements:
+
+// Prompt the user to enter the number of elements (size of the array).
+
+// Read n integer elements from the user and store them in an array.
+
+// Use Bubble Sort to sort the array in ascending order.
+
+// Display the array before and after sorting.
+
+// Constraints:
+
+// The array size n must be a positive integer.
+
+// Elements can include both positive and negative integers.
+
+// Sample Input/Output:
+// Enter size : 5  
+// Enter value: 9  
+// Enter value: 4  
+// Enter value: 7  
+// Enter value: 1  
+// Enter value: 3  
+
+// Entered Elements:  9 4 7 1 3  
+// Elements after bubble sort:  1 3 4 7 9  
+
 #include <iostream>
 using namespace std;
 
