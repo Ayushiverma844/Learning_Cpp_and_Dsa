6edea3fd3555d9b5c1061ab638d9c1abe95dca12
Ayushi Verma <ayushiverma844@gmail.com>
2025-05-21 07:51:05 +0000

    container config done

--- CPP/3__FUNCTIONS/5_Prime.cpp
+++ CPP/3__FUNCTIONS/5_Prime.cpp
@@ -13,13 +13,14 @@
 using namespace std;
 
 //function to check prime
-bool is_prime(int n){
-  for(int i=2;i<n;i++){
-    if(n%i==0)
-      return 0; //false(not prime)
-  }
-   return 1;   //true(prime)
-} 
+bool is_prime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i*i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 
 int main(){
 int n;
