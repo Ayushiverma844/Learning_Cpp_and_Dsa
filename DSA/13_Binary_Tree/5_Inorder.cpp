bce004ce2432f781f887a1cb1329b9588cb25818
Ayushi Verma <ayushiverma844@gmail.com>
2025-06-05 03:32:01 +0000

    container config done

---
+++ DSA/13_Binary_Tree/5_Inorder.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node* left;
+Node* right;
+
+Node(int val){
+    data = val;
+    left = right = NULL;
+}
+};
+
+int idx = -1;
+Node* buildTree(vector<int> preorder){
+    idx++;
+        // Base case: if the current value is -1, it represents a NULL node
+    if (preorder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder); //left
+    root->right = buildTree(preorder); //right
+
+    return root;
+
+}
+
+//inorder
+void inorder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+
+    inorder(root->left);
+    cout<< root->data<<" ";
+    inorder(root->right);
+}
+
+int main(){
+    vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1} ;
+    Node* root = buildTree(preorder);
+
+    inorder(root);
+    cout<<endl;
+
+    return 0;
+}
