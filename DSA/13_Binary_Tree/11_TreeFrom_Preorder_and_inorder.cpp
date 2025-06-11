1077d6cb424b554f11dc0a7f93093b378e3a7ae0
Ayushi Verma <ayushiverma844@gmail.com>
2025-06-11 04:40:15 +0000

    container config done

---
+++ DSA/13_Binary_Tree/11_TreeFrom_Preorder_and_inorder.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Definition of Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+public:
+    // Function to find the index of a value in inorder array
+    int search(vector<int>& inorder, int left, int right, int val) {
+        for (int i = left; i <= right; i++) {
+            if (inorder[i] == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Recursive helper function to build the tree
+    Node* helper(vector<int>& preorder, vector<int>& inorder, int& preIdx, int left, int right) {
+        if (left > right) {
+            return NULL;
+        }
+
+        // Create root node with current preorder value
+        Node* root = new Node(preorder[preIdx]);
+
+        // Find index of current root in inorder array
+        int inIdx = search(inorder, left, right, preorder[preIdx]);
+        preIdx++;
+
+        // Recursively build left and right subtrees
+        root->left = helper(preorder, inorder, preIdx, left, inIdx - 1);
+        root->right = helper(preorder, inorder, preIdx, inIdx + 1, right);
+
+        return root;
+    }
+
+    // Main function to be called to build tree
+    Node* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int preIdx = 0;
+        return helper(preorder, inorder, preIdx, 0, inorder.size() - 1);
+    }
+};
+
+// Function to print inorder traversal of the tree
+void printInorder(Node* root) {
+    if (root == NULL) return;
+    printInorder(root->left);
+    cout << root->data << " ";
+    printInorder(root->right);
+}
+
+// Driver code
+int main() {
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+
+    Solution sol;
+    Node* root = sol.buildTree(preorder, inorder);
+
+    cout << "Inorder Traversal of Constructed Tree: ";
+    printInorder(root);  // Should match the input inorder
+    cout << endl;
+
+    return 0;
+}
