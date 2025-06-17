d2afdecb71fa08c057fd2f7bea8704e8977edfa4
Ayushi Verma <ayushiverma844@gmail.com>
2025-06-17 07:26:43 +0000

    container config done

--- DSA/13_Binary_Tree/1.Build_Tree.cpp
+++ DSA/13_Binary_Tree/1.Build_Tree.cpp
@@ -1,3 +1,46 @@
+// Problem Statement:
+
+// Write a C++ program to build a binary tree from a given preorder traversal array where -1 represents a NULL node. 
+// Implement a function that constructs the binary tree using recursion, and then display 
+// the root, left child, and right child values of the constructed binary tree.
+
+// Example:
+
+// Given preorder traversal array:
+// {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1}
+
+// This represents the following binary tree:
+
+//         1
+//        / \
+//       2   3
+//          / \
+//         4   5
+
+// Explanation:
+
+// 1 is the root.
+
+// 2 is the left child of 1.
+
+// -1, -1 means 2 has no left or right children.
+
+// 3 is the right child of 1.
+
+// 4 is the left child of 3.
+
+// -1, -1 means 4 has no children.
+
+// 5 is the right child of 3.
+
+// -1, -1 means 5 has no children.
+
+// Output:
+// 1
+// 2
+// 3
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
